39903c289c47d8905ab90adae0a9be62c83f8127
kazuhi-ra <kazuhira4126@gmail.com>
2022-07-02 01:13:11 +0900

    再帰の練習9

--- merge.ml
+++ merge.ml
@@ -1,21 +1,16 @@
-let rec insert lst n =
-  match lst with
-  | [] -> [ n ]
-  | first :: rest -> if first > n then n :: lst else first :: insert rest n
-
-let rec ins_sort lst =
-  match lst with [] -> [] | first :: rest -> insert (ins_sort rest) first
-
-let rec append lst1 lst2 =
-  match lst1 with [] -> lst2 | first :: rest -> first :: append rest lst2
-
 let rec merge lst1 lst2 =
-  match lst1 with [] -> lst2 | _ -> ins_sort (append lst1 lst2)
+  match (lst1, lst2) with
+  | [], [] -> []
+  | [], first :: rest -> lst2
+  | first :: rest, [] -> lst1
+  | first1 :: rest1, first2 :: rest2 ->
+      if first1 < first2 then first1 :: merge rest1 lst2
+      else first2 :: merge lst1 rest2
 ;;
 
 merge [] [] = [];;
-merge [ 1 ] [] = [ 1 ];;
 merge [] [ 1 ] = [ 1 ];;
+merge [ 1 ] [] = [ 1 ];;
 merge [ 1; 2 ] [] = [ 1; 2 ];;
 merge [ 1; 3 ] [ 2; 4 ] = [ 1; 2; 3; 4 ];;
 merge [ 2; 4 ] [ 1; 3 ] = [ 1; 2; 3; 4 ];;
