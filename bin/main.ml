33a9e099c0e1f9088158a234b7a4c362e8e430f4
kazuhi-ra <kazuhira4126@gmail.com>
2022-07-02 22:33:38 +0900

    再帰の練習11

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,9 @@
-open Lib
+open Lib.Ekimei
 
-let () = print_string Unchi.unchi
+let rec romaji_to_kanji romaji global_ekimei_list =
+  match global_ekimei_list with
+  | [] -> ""
+  | first :: rest ->
+      if first.romaji = romaji then first.kanji else romaji_to_kanji romaji rest
+
+let () = print_string (romaji_to_kanji "asakusa" global_ekimei_list)
