a2adca907d8612051e772db0925864e148d436b3
kazuhi-ra <kazuhira4126@gmail.com>
2022-07-02 23:44:12 +0900

    再帰の練習12

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,7 @@
-open Lib.Ekimei
-open Lib.Romaji_to_kanji
+open Lib.Get_ekikan_kyori
+open Lib.Ekikan
 
-let () = print_string (romaji_to_kanji "asakusa" global_ekimei_list)
+let test1 = get_ekikan_kyori "茗荷谷" "新大塚" global_ekikan_list = 1.2
+let test2 = get_ekikan_kyori "茗荷谷" "池袋" global_ekikan_list = infinity
+let test3 = get_ekikan_kyori "東京" "大手町" global_ekikan_list = 0.6
+let () = print_int (if test1 && test2 && test3 then 1 else 0)
