2069dec3d4254691adef76f0005feff6135f16cf
kazuhi-ra <kazuhira4126@gmail.com>
2022-07-03 21:29:34 +0900

    再帰の練習13

--- bin/main.ml
+++ bin/main.ml
@@ -1,23 +1,4 @@
-open Lib.Kyori_wo_hyoji
-open Lib.Ekikan
+open Lib.Fac
 
-let test1 =
-  kyori_wo_hyoji "myougadani" "shinotsuka" global_ekikan_list
-  = "myougadani という駅は存在しません"
-
-let test2 =
-  kyori_wo_hyoji "myogadani" "shinotsuka" global_ekikan_list
-  = "茗荷谷から新大塚までは 1.2 キロです"
-
-let test3 =
-  kyori_wo_hyoji "myogadani" "ikebukuro" global_ekikan_list = "茗荷谷と池袋はつながっていません"
-
-let test4 =
-  kyori_wo_hyoji "tokyo" "ootemachi" global_ekikan_list
-  = "ootemachi という駅は存在しません"
-
-let test5 =
-  kyori_wo_hyoji "tokyo" "otemachi" global_ekikan_list = "東京から大手町までは 0.6 キロです"
-
-let kekka = test1 && test2 && test3 && test4 && test5
+let kekka = fac 0 = 1 && fac 1 = 1 && fac 4 = 24 && fac 10 = 3628800
 let () = print_int (if kekka then 1 else 0)
---
+++ lib/fac.ml
@@ -0,0 +1,1 @@
+let rec fac i = if i = 0 then 1 else i * fac (i - 1)
