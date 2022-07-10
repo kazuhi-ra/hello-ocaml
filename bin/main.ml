ffee61095983d284055c195ba58bc9036c93f53d
kazuhi-ra <kazuhira4126@gmail.com>
2022-07-10 15:28:46 +0900

    再帰の練習19

--- bin/main.ml
+++ bin/main.ml
@@ -1,28 +1,104 @@
-open Lib.Saitanro
-open Lib.Make_eki_list
+open Lib.Ekimei
+open Lib.Seiretsu
 
-let eki_list =
+let ekimei_list =
   [
-    { namae = "池袋"; saitan_kyori = infinity; temae_list = [] };
-    { namae = "新大塚"; saitan_kyori = infinity; temae_list = [] };
-    { namae = "茗荷谷"; saitan_kyori = infinity; temae_list = [] };
-    { namae = "後楽園"; saitan_kyori = infinity; temae_list = [] };
-    { namae = "本郷三丁目"; saitan_kyori = infinity; temae_list = [] };
-    { namae = "御茶ノ水"; saitan_kyori = infinity; temae_list = [] };
+    { kanji = "池袋"; kana = "いけぶくろ"; romaji = "ikebukuro"; shozoku = "丸ノ内線" };
+    { kanji = "新大塚"; kana = "しんおおつか"; romaji = "shinotsuka"; shozoku = "丸ノ内線" };
+    { kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸ノ内線" };
+    { kanji = "後楽園"; kana = "こうらくえん"; romaji = "korakuen"; shozoku = "丸ノ内線" };
+    {
+      kanji = "本郷三丁目";
+      kana = "ほんごうさんちょうめ";
+      romaji = "hongosanchome";
+      shozoku = "丸ノ内線";
+    };
+    { kanji = "御茶ノ水"; kana = "おちゃのみず"; romaji = "ochanomizu"; shozoku = "丸ノ内線" };
   ]
 
-let test1 = shokika [] "茗荷谷" = []
-
-let test2 =
-  shokika eki_list "茗荷谷"
+let test1 =
+  ekimei_insert []
+    { kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸ノ内線" }
   = [
-      { namae = "池袋"; saitan_kyori = infinity; temae_list = [] };
-      { namae = "新大塚"; saitan_kyori = infinity; temae_list = [] };
-      { namae = "茗荷谷"; saitan_kyori = 0.; temae_list = [ "茗荷谷" ] };
-      { namae = "後楽園"; saitan_kyori = infinity; temae_list = [] };
-      { namae = "本郷三丁目"; saitan_kyori = infinity; temae_list = [] };
-      { namae = "御茶ノ水"; saitan_kyori = infinity; temae_list = [] };
+      { kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸ノ内線" };
     ]
 
-let kekka = test1 && test2
+let test2 =
+  ekimei_insert
+    [
+      { kanji = "池袋"; kana = "いけぶくろ"; romaji = "ikebukuro"; shozoku = "丸ノ内線" };
+      {
+        kanji = "御茶ノ水";
+        kana = "おちゃのみず";
+        romaji = "ochanomizu";
+        shozoku = "丸ノ内線";
+      };
+      {
+        kanji = "新大塚";
+        kana = "しんおおつか";
+        romaji = "shinotsuka";
+        shozoku = "丸ノ内線";
+      };
+      {
+        kanji = "本郷三丁目";
+        kana = "ほんごうさんちょうめ";
+        romaji = "hongosanchome";
+        shozoku = "丸ノ内線";
+      };
+      { kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸ノ内線" };
+    ]
+    { kanji = "後楽園"; kana = "こうらくえん"; romaji = "korakuen"; shozoku = "丸ノ内線" }
+  = [
+      { kanji = "池袋"; kana = "いけぶくろ"; romaji = "ikebukuro"; shozoku = "丸ノ内線" };
+      {
+        kanji = "御茶ノ水";
+        kana = "おちゃのみず";
+        romaji = "ochanomizu";
+        shozoku = "丸ノ内線";
+      };
+      { kanji = "後楽園"; kana = "こうらくえん"; romaji = "korakuen"; shozoku = "丸ノ内線" };
+      {
+        kanji = "新大塚";
+        kana = "しんおおつか";
+        romaji = "shinotsuka";
+        shozoku = "丸ノ内線";
+      };
+      {
+        kanji = "本郷三丁目";
+        kana = "ほんごうさんちょうめ";
+        romaji = "hongosanchome";
+        shozoku = "丸ノ内線";
+      };
+      { kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸ノ内線" };
+    ]
+
+let test3 = seiretsu [] = []
+
+let test4 =
+  seiretsu ekimei_list
+  = [
+      { kanji = "池袋"; kana = "いけぶくろ"; romaji = "ikebukuro"; shozoku = "丸ノ内線" };
+      {
+        kanji = "御茶ノ水";
+        kana = "おちゃのみず";
+        romaji = "ochanomizu";
+        shozoku = "丸ノ内線";
+      };
+      { kanji = "後楽園"; kana = "こうらくえん"; romaji = "korakuen"; shozoku = "丸ノ内線" };
+      {
+        kanji = "新大塚";
+        kana = "しんおおつか";
+        romaji = "shinotsuka";
+        shozoku = "丸ノ内線";
+      };
+      {
+        kanji = "本郷三丁目";
+        kana = "ほんごうさんちょうめ";
+        romaji = "hongosanchome";
+        shozoku = "丸ノ内線";
+      };
+      { kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myogadani"; shozoku = "丸ノ内線" };
+    ]
+
+let kekka = test1 && test2 && test3 && test4
 let () = print_string (if kekka then " Success" else "Failed")
