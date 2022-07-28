dce3f57333104cfab760aa1a959b2fba314c7024
kazuhi-ra <kazuhira4126@gmail.com>
2022-07-29 00:17:48 +0900

    未確定の駅 q を必要に応じて更新した駅を返す

---
+++ lib/koushin1.ml
@@ -0,0 +1,16 @@
+open Get_ekikan_kyori
+open Make_eki_list
+open Ekikan
+
+let koushin1 p q =
+  let ekikan_kyori = get_ekikan_kyori p.namae q.namae global_ekikan_list in
+  if ekikan_kyori = infinity then q
+  else
+    let new_saitan_kyori = p.saitan_kyori +. ekikan_kyori in
+    if new_saitan_kyori >= q.saitan_kyori then q
+    else
+      {
+        namae = q.namae;
+        saitan_kyori = new_saitan_kyori;
+        temae_list = q.namae :: p.temae_list;
+      }
