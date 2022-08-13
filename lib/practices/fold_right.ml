cb9a2586d684c61a278e2d1b5bf072628bddf73c
kazuhi-ra <kazuhira4126@gmail.com>
2022-08-13 18:52:30 +0900

    無名関数を使って書き換え

--- lib/practices/fold_right.ml
+++ lib/practices/fold_right.ml
@@ -3,17 +3,6 @@ let rec fold_right f lst init =
   | [] -> init
   | first :: rest -> f first (fold_right f rest init)
 
-let sum lst =
-  (* match lst with [] -> 0 | first :: rest -> first + sum rest *)
-  let fn n m = n + m in
-  fold_right fn lst 0
-
-let length lst =
-  (* match lst with [] -> 0 | _ :: rest -> 1 + length rest *)
-  let fn _ m = 1 + m in
-  fold_right fn lst 0
-
-let append lst1 lst2 =
-  (* match lst1 with [] -> lst2 | first :: rest -> first :: append rest lst2 *)
-  let fn l1 l2 = l1 :: l2 in
-  fold_right fn lst1 lst2
+let sum lst = fold_right (fun n m -> n + m) lst 0
+let length lst = fold_right (fun _ m -> 1 + m) lst 0
+let append lst1 lst2 = fold_right (fun l1 l2 -> l1 :: l2) lst1 lst2
