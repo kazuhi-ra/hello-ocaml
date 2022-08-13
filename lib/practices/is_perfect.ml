f9e5a88aa15c08348070ff63dcef2c738e5c8531
kazuhi-ra <kazuhira4126@gmail.com>
2022-08-14 00:08:10 +0900

    名前変更

--- lib/practices/is_perfect.ml
+++ lib/practices/is_perfect.ml
@@ -2,6 +2,6 @@ let divisor n =
   let rec enumerate n = if n = 1 then [ 1 ] else n :: enumerate (n - 1) in
   List.filter (fun x -> n mod x = 0) (enumerate n)
 
-let perfect n =
+let is_perfect n =
   let sum = List.fold_right ( + ) (divisor n) 0 in
   n * 2 = sum
