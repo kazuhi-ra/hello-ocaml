3375ecb45d49eee0708c342e0ac57ec4be778d3a
kazuhi-ra <kazuhira4126@gmail.com>
2022-08-13 20:28:21 +0900

    infix関数をprefix関数に変換

--- lib/practices/fold_right.ml
+++ lib/practices/fold_right.ml
@@ -3,6 +3,6 @@ let rec fold_right f lst init =
   | [] -> init
   | first :: rest -> f first (fold_right f rest init)
 
-let sum lst = fold_right (fun n m -> n + m) lst 0
+let sum lst = fold_right ( + ) lst 0
 let length lst = fold_right (fun _ m -> 1 + m) lst 0
 let append lst1 lst2 = fold_right (fun l1 l2 -> l1 :: l2) lst1 lst2
