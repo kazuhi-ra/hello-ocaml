ac9744b05fd1c8e8e3a4e3c3f5a16ae6e9545233
kazuhi-ra <kazuhira4126@gmail.com>
2022-08-20 22:48:17 +0900

    fold_rightを使って書き直し

--- lib/saitan_wo_bunri.ml
+++ lib/saitan_wo_bunri.ml
@@ -1,11 +1,24 @@
 open Make_eki_list
 
-let rec saitan_wo_bunri eki_list =
-  match eki_list with
-  | [] -> ({ namae = ""; saitan_kyori = infinity; temae_list = [] }, [])
-  | first :: rest ->
-      let bunri_eki, bunri_list = saitan_wo_bunri rest in
-      if bunri_eki.namae = "" then (first, [])
-      else if first.saitan_kyori < bunri_eki.saitan_kyori then
-        (first, bunri_eki :: bunri_list)
-      else (bunri_eki, first :: bunri_list)
+let rec fold_right fn lst init =
+  match lst with
+  | [] -> init
+  | first :: rest -> fn first (fold_right fn rest init)
+
+let saitan_wo_bunri eki_list =
+  let init_eki = { namae = ""; saitan_kyori = infinity; temae_list = [] } in
+  fold_right
+    (fun current (result_eki, result_eki_lst) ->
+      if result_eki = init_eki then (current, [])
+      else if current.saitan_kyori < result_eki.saitan_kyori then
+        (current, result_eki :: result_eki_lst)
+      else (result_eki, current :: result_eki_lst))
+    eki_list (init_eki, [])
+(* match eki_list with
+   | [] -> ({ namae = ""; saitan_kyori = infinity; temae_list = [] }, [])
+   | first :: rest ->
+       let bunri_eki, bunri_list = saitan_wo_bunri rest in
+       if bunri_eki.namae = "" then (first, [])
+       else if first.saitan_kyori < bunri_eki.saitan_kyori then
+         (first, bunri_eki :: bunri_list)
+       else (bunri_eki, first :: bunri_list) *)
