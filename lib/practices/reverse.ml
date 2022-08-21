e792c74a8c5c0395b1d3009f357d5273d801e8dd
kazuhi-ra <kazuhira4126@gmail.com>
2022-08-21 19:58:51 +0900

    アキュムレータを使って書き直す

--- lib/practices/reverse.ml
+++ lib/practices/reverse.ml
@@ -1,2 +1,9 @@
-let rec reverse lst =
-  match lst with [] -> [] | first :: rest -> reverse rest @ [ first ]
+let reverse lst =
+  let rec rev lst result =
+    match lst with [] -> result | first :: rest -> rev rest (first :: result)
+  in
+  rev lst []
+(* match lst with
+   | [] -> []
+   | first :: rest ->
+     (reverse rest) @ [first] *)
