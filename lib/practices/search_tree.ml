a142b9059fc3203200f65b128edee3a031a53f1d
kazuhi-ra <kazuhira4126@gmail.com>
2022-08-28 23:35:15 +0900

    名前修正

--- lib/practices/search_tree.ml
+++ lib/practices/search_tree.ml
@@ -7,7 +7,7 @@ let rec search_tree tree n =
   match tree with
   | Empty -> false
   | Leaf x -> n = x
-  | Node (left_tree, x, right_tree2) ->
+  | Node (left_tree, x, right_tree) ->
       if x = n then true
       else if n < x then search_tree left_tree n
-      else search_tree right_tree2 n
+      else search_tree right_tree n
